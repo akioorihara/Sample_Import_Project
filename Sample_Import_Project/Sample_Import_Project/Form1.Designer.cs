262a88d916474e4f6ea7fc5d7b09a7670232a455
nsawyer <nsawyer@cdhts.com>
2021-06-23 09:12:12 -0400

    Create Sample Import Form in Project

---
+++ Sample_Import_Project/Sample_Import_Project/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Sample_Import_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImportFilePathTextbox = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.ImportFileDataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClearScreenButton = new System.Windows.Forms.Button();
+            this.ImportFileButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ImportFileDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImportFilePathTextbox
+            // 
+            this.ImportFilePathTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImportFilePathTextbox.Location = new System.Drawing.Point(15, 49);
+            this.ImportFilePathTextbox.Name = "ImportFilePathTextbox";
+            this.ImportFilePathTextbox.Size = new System.Drawing.Size(580, 20);
+            this.ImportFilePathTextbox.TabIndex = 0;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseButton.Location = new System.Drawing.Point(601, 47);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(76, 23);
+            this.BrowseButton.TabIndex = 1;
+            this.BrowseButton.Text = "Browse...";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // ImportFileDataGridView
+            // 
+            this.ImportFileDataGridView.AllowUserToAddRows = false;
+            this.ImportFileDataGridView.AllowUserToDeleteRows = false;
+            this.ImportFileDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImportFileDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ImportFileDataGridView.Location = new System.Drawing.Point(12, 132);
+            this.ImportFileDataGridView.Name = "ImportFileDataGridView";
+            this.ImportFileDataGridView.ReadOnly = true;
+            this.ImportFileDataGridView.Size = new System.Drawing.Size(664, 400);
+            this.ImportFileDataGridView.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Import File Data:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "File Path to Import File:";
+            // 
+            // ClearScreenButton
+            // 
+            this.ClearScreenButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClearScreenButton.Location = new System.Drawing.Point(601, 76);
+            this.ClearScreenButton.Name = "ClearScreenButton";
+            this.ClearScreenButton.Size = new System.Drawing.Size(76, 23);
+            this.ClearScreenButton.TabIndex = 5;
+            this.ClearScreenButton.Text = "Clear Screen";
+            this.ClearScreenButton.UseVisualStyleBackColor = true;
+            this.ClearScreenButton.Click += new System.EventHandler(this.ClearScreenButton_Click);
+            // 
+            // ImportFileButton
+            // 
+            this.ImportFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImportFileButton.Location = new System.Drawing.Point(520, 76);
+            this.ImportFileButton.Name = "ImportFileButton";
+            this.ImportFileButton.Size = new System.Drawing.Size(75, 23);
+            this.ImportFileButton.TabIndex = 6;
+            this.ImportFileButton.Text = "Import File";
+            this.ImportFileButton.UseVisualStyleBackColor = true;
+            this.ImportFileButton.Click += new System.EventHandler(this.LoadFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 544);
+            this.Controls.Add(this.ImportFileButton);
+            this.Controls.Add(this.ClearScreenButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ImportFileDataGridView);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.ImportFilePathTextbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ImportFileDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ImportFilePathTextbox;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.DataGridView ImportFileDataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ClearScreenButton;
+        private System.Windows.Forms.Button ImportFileButton;
+    }
+}
+
---
+++ Sample_Import_Project/Sample_Import_Project/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Sample_Import_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+
+            // The OpenFileDialog allows us to pop-up a new window where the user can select a file from their computer
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set some properties on the Open File dialog before we display it to the user
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv| All Files (*.*)|*.*";
+            openFileDialog.Title = "Select File for Import";
+
+            // Display the Open File dialog to the user
+            // The function ShowDialog() stops all code excution until the dialog is closed by the user
+            // We store the result of the Open File Dialog to a variable so we can act on it
+            DialogResult openFileDialogResult = openFileDialog.ShowDialog();
+
+            // Check that the result of the Open File Dialog is OK.  expected results are OK or Cancel
+            if (openFileDialogResult == DialogResult.OK)
+            {
+                // If OK, we retrieve the File the user selected in the Open File Dialog, and put that into the Import File Path Textbox on the main Form
+                ImportFilePathTextbox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void LoadFileButton_Click(object sender, EventArgs e)
+        {
+            // Pull out the Import File Path from the textbox into a local variable
+            string ImportFilePath = ImportFilePathTextbox.Text;
+
+            // Perform some validation on the Import File Path Provided
+            // Validate Import File Path is not empty
+            if (ImportFilePath.Trim().Length == 0)
+            {
+                MessageBox.Show("The Import file Path is empty!");
+                return;
+            }
+
+            // Validate the Import file Path is a valid path on the local machine
+            if (System.IO.File.Exists(ImportFilePath) == false)
+            {
+                MessageBox.Show("The Import file at the provided path does not exist!");
+                return;
+            }
+
+            // Validate that the Import File Path is a CSV file
+            if (System.IO.Path.GetExtension(ImportFilePath) != ".csv")
+            {
+                MessageBox.Show("The Import file is not a valid type.  Valid types are: CSV");
+                return;
+            }
+
+
+            // Validation complete
+            // Begin Import of the file using our own library ADSFramework.Excel
+            DataTable ImportFileDataTable = ADSFramework.Excel.CsvConverter.ToDatatable(ImportFilePath);
+
+            // Do some validation on the Import File DataTable
+            // Validate DataTable has 1+ columns
+            if (ImportFileDataTable.Columns.Count == 0)
+            {
+                MessageBox.Show("The Import File does not contain any columns!");
+                return;
+            }
+
+            // Validate DataTable has 1+ Rows
+            if (ImportFileDataTable.Rows.Count == 0)
+            {
+                MessageBox.Show("The Import File does not contain any Rows!");
+                return;
+            }
+
+            // Completed Import File DataTable validation
+            // Now to finally display the Import File data to the user
+            // The DataTable object has no way to show itself on the Form to the user
+            // We use the DataGridView Control to display the DataTable by setting the DataSource
+            // (Controls are objects which are shown in the Form such as TextBox, Label, Button, DataGridView, etc.  All controls inherit from System.Windows.Forms.Control)
+            ImportFileDataGridView.DataSource = ImportFileDataTable;
+            MessageBox.Show("File has been Imported Successfully!");
+        }
+
+        private void ClearScreenButton_Click(object sender, EventArgs e)
+        {
+            // Clearing the screen is a simple as overwriting properties with empty string/null
+            ImportFilePathTextbox.Text = "";
+            ImportFileDataGridView.DataSource = null;
+            MessageBox.Show("Import Screen has been Cleared!");
+        }
+    }
+}
